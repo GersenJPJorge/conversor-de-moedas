782bb7529a50b5ec5bc604f32d55c7c2318650f1
Gersen James <gersen.jorge@gmail.com>
2019-04-08 16:06:30 -0300

    Implementando a ação de conversão de moedas

---
+++ src/app/conversor/conversor.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ConversorComponent } from './components';
+import { MoedaService, ConversorService } from './services';
+import { HttpClientModule } from '@angular/common/http';
+import { NumeroDirective } from './directives';
+import { ModalCotacaoComponent } from './utils';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  declarations: [
+  	ConversorComponent,
+  	NumeroDirective,
+  	ModalCotacaoComponent,
+  ],
+  exports: [                                              // se usar rotas tem que ter o exports
+  	ConversorComponent,
+  ],
+  providers: [
+  	MoedaService,
+    ConversorService,
+  ]
+})
+export class ConversorModule { }
